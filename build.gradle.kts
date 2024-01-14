c9327cf05b2ae89fd2f3fb472911c15f61df5ed9
Weh, Selina <weh@infosim.net>
2024-01-14 14:06:35 +0100

    added build dependencys

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.guava:guava:33.0.0-jre")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.16.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
 
 tasks.test {
