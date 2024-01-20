1e56ff2bc206230ec6b2de64ed13a7de5d83d3ba
Weh, Selina <weh@infosim.net>
2024-01-20 11:08:49 +0100

    added signature verifier

--- src/main/java/autonomousVehicle/camera/CameraBuilder.java
+++ src/main/java/autonomousVehicle/camera/CameraBuilder.java
@@ -1,7 +1,8 @@
 package autonomousVehicle.camera;
 
 import configuration.Configuration;
-import configuration.SignatureVerifier;
+import exceptions.SignatureVerificationException;
+import signatureVerification.SignatureVerifier;
 
 import java.io.File;
 import java.net.URL;
@@ -16,23 +17,18 @@ public class CameraBuilder {
         cameraPorts = new ArrayList<>(Configuration.INSTANCE.numberOfCameras);
     }
 
-    public ArrayList<Object> buildCameras(String pathToJar) {
-        /*
+    public ArrayList<Object> buildCameras(String pathToJar) throws SignatureVerificationException {
+
         SignatureVerifier signatureVerifier = new SignatureVerifier();
         if (!signatureVerifier.verifySign(pathToJar)) {
-            System.out.println("JAR file not signed");
-            return null;
+            throw new SignatureVerificationException("JAR file not signed");
         }
-
-         */
         try {
             for (int i = 0; i < Configuration.INSTANCE.numberOfCameras; i++) {
-                System.out.println("pathToJar | " + pathToJar);
                 URL[] urls = {new File(pathToJar).toURI().toURL()};
 
                 URLClassLoader urlClassLoader = new URLClassLoader(urls, CameraBuilder.class.getClassLoader());
                 Class<?> cameraClass = Class.forName("Camera", true, urlClassLoader);
-                System.out.println("cameraClass     : " + cameraClass);
                 Object cameraInstance = cameraClass.getMethod("getInstance").invoke(null);
 
                 Object cameraPort = cameraClass.getDeclaredField("port").get(cameraInstance);
